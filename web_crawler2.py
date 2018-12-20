f1a94cc3ac4be22af92f521341ca2f6a4156c6bc
gsmm007 <42911618+gsmm007@users.noreply.github.com>
2018-12-20 19:56:45 +0530

    Update web_crawler2.py

--- web_crawler2.py
+++ web_crawler2.py
@@ -1,13 +1,19 @@
 #My_First_Project
 import os
+import requets
+from bs4 import BeautifulSoup
+
 #https://play.typeracer.com
+
 '''creating new directory
-assigning each directory to a new website/webpage
-'''
+assigning each directory to a new website/webpage'''
+
 def new_directories(new_directory):
 	if not.os.path.exixts(new_directory):
 		os.mkdir(new_directory)
+		
 #queue is nothing but a waiting list of the links on the main webpage.
+
 def data_files(project, url) :
 	queue = project + '/queue.txt'
 	crawled = project + '/crawled.txt'
@@ -21,5 +27,13 @@ def write_file(path, data):
 	f.write(data)
 	f.close()
 
-	
+def addtofile(path,data):
+	f = open(path, 'w')
+	f.write(data + '\n')
+
+def deletecontents(path):
+	f = open(path, 'w')
+	f.close()
+
+
 
