c5244e8a506636d60afb642b8450ceb658e9b60a
gsmm007 <42911618+gsmm007@users.noreply.github.com>
2018-12-20 19:31:27 +0530

    storing waitng list

---
+++ web_crawler2.py
@@ -0,0 +1,25 @@
+#My_First_Project
+import os
+#https://play.typeracer.com
+'''creating new directory
+assigning each directory to a new website/webpage
+'''
+def new_directories(new_directory):
+	if not.os.path.exixts(new_directory):
+		os.mkdir(new_directory)
+#queue is nothing but a waiting list of the links on the main webpage.
+def data_files(project, url) :
+	queue = project + '/queue.txt'
+	crawled = project + '/crawled.txt'
+	if not os.path.isfile(queue):
+		write_file(queue, url)
+	if not os.path.isfile(crawled):
+		write_file(crawled, url)
+
+def write_file(path, data):
+	f = open(path, 'w')
+	f.write(data)
+	f.close()
+
+	
+
